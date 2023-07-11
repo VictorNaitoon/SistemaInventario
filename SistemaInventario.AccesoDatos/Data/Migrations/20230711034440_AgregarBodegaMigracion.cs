e0104d9e52e12cad9aecff3391c5f6aed7e5262e
VictorNaitoon <victormolinalvp@gmail.com>
2023-07-11 00:48:08 -0300

    Configuración del Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/Migrations/20230711034440_AgregarBodegaMigracion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarBodegaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bodega",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bodega", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bodega");
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Nombre es requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser de máximo de 60 carácteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="Descripcion es requerida")]
+        [MaxLength(150, ErrorMessage ="Descripción debe ser de máximo de 150 carácteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
