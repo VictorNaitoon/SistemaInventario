3b6015f6584a9628a09c72627ea6720827ce5d7e
VictorNaitoon <victormolinalvp@gmail.com>
2023-07-13 23:57:43 -0300

    Patron de repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        //Para hacer los métodos asincronos ponemos el Task, menos para los métodos Remover
+        Task<T> Obtener(int id);
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T, bool>> filtro = null,    //Recibe una expresio que funciona como filtro para empezar a filtrar la lista
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, //Ordena la lista. 
+            string incluirPropiedades = null, // este se encarga de hacer todos los enlaces con otros objetos
+            bool isTracking = true //Esto es cuando querramos acceder a un objeto o una lista de objetos y al mismo tiempo lo queremos modificar
+            //Siempre será true pero cuando sea false es cuando estemos trabajando con el mismo objeto
+            );
+        Task<T> ObtenerPrimero(
+            Expression<Func<T, bool>> filtro = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+        Task Agregar(T entidad);
+        void Remover(T entidad);
+        void RemoverRango(IEnumerable<T> entidad);
+
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); //de esta manera estoy seteando al objeto y lo pongo como una propiedad dbSet
+        }
+        public async Task Agregar(T entidad)
+        {
+            await dbSet.AddAsync(entidad); 
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await dbSet.FindAsync(id); 
+        }
+
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirPorp in incluirPropiedades.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)) //Lo hacemos primero de tipo char con el spli para que sea un arreglo de char y que cada vez que vez que encuentre una coma lo separa en un caracter  luego utilizo el stringOptions y removemos los espacios vacios
+                {
+                    query = query.Include(incluirPorp); //este include de entity framework hace que si trabajamos con objetos relacionados a este objeto incluira las propiedades de los objetos relacionados                         
+                }
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirPropiedades);
+                }
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Remover(T entidad)
+        {
+            dbSet.Remove(entidad);
+        }
+
+        public void RemoverRango(IEnumerable<T> entidad)
+        {
+            dbSet.RemoveRange(entidad);
+        }
+    }
+}
